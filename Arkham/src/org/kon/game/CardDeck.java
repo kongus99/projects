074603f60d0f4881e8d79ca498f9c1136014e9e6
kongus99 <kongus99@gmail.com>
2013-01-03 10:12:09 +0100

    Card added

---
+++ Arkham/src/org/kon/game/CardDeck.java
@@ -0,0 +1,19 @@
+package org.kon.game;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ldudek
+ * Date: 1/3/13
+ * Time: 9:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardDeck {
+    public CardDeck(List<Card> initialCards) {
+    }
+
+    public Card draw() {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+}
---
+++ Arkham/tst/org/kon/game/CardDeckTest.java
@@ -0,0 +1,33 @@
+package org.kon.game;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ldudek
+ * Date: 1/3/13
+ * Time: 9:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CardDeckTest {
+    //    @Test
+//    public void whenCardDeckIsNotShuffledWeDrawCardsInTheSameSequenceTheyWerePutIntoDeck() throws Exception {
+//        List<Card> initialCards = new ArrayList<Card>();
+//        initialCards.add(new Card());
+//        initialCards.add(new Card());
+//        initialCards.add(new Card());
+//        CardDeck deck = new CardDeck(initialCards);
+//        List<Card> cards = new ArrayList<Card>();
+//        Card c = deck.draw();
+//        do {
+//            cards.add(c = deck.draw());
+//        }
+//        while (c!= null);
+//
+//        Assert.assertArrayEquals(initialCards.toArray(),);
+//    }
+    @Test
+    public void whenCardDeckIsNotShuffledWeDrawCardsInTheSameSequenceTheyWerePutIntoDeck() throws Exception {
+
+    }
+}
