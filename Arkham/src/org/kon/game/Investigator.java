da8013429037591e2797a00a4c490dede365e610
kongus99 <kongus99@gmail.com>
2013-01-10 09:17:19 +0100

    Deck unified

---
+++ Arkham/src/org/kon/game/Investigator.java
@@ -0,0 +1,28 @@
+package org.kon.game;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ldudek
+ * Date: 1/10/13
+ * Time: 9:01 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Investigator {
+    public Investigator(List<Possession> fixedPossessions) {
+        for (Possession fixedPossession : fixedPossessions) {
+
+        }
+
+
+    }
+
+    public void assignFixedPossessions(Deck<Possession> deck) {
+
+    }
+
+    public boolean has(Possession possession) {
+        return false;
+    }
+}
--- Arkham/tst/org/kon/game/InvestigatorTest.java
+++ Arkham/tst/org/kon/game/InvestigatorTest.java
@@ -1,6 +1,11 @@
 package org.kon.game;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created with IntelliJ IDEA.
@@ -12,7 +17,20 @@ import org.junit.Test;
 public class InvestigatorTest {
     @Test
     public void investigatorShouldHaveListOfFixedPossessions() throws Exception {
-
-
+        List<Possession> possessions = new ArrayList<Possession>();
+        Possession p1 = new Possession();
+        Possession p2 = new Possession();
+        Possession p3 = new Possession();
+        Possession p4 = new Possession();
+        possessions.add(p1);
+        possessions.add(p2);
+        Investigator investigator = new Investigator(p2,p4);
+        possessions.add(p3);
+        possessions.add(p4);
+        Random seed = new Random();
+        Deck<Possession> deck = new Deck<Possession>(possessions,seed);
+        investigator.assignFixedPossessions(deck);
+        Assert.assertTrue(investigator.has(p2));
+        Assert.assertTrue(investigator.has(p4));
     }
 }
